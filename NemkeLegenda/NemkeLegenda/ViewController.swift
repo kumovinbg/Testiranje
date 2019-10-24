0a496042cec45f797ef03ababf30a4a6dcaf0083
Nikola Lukovic <lukovic89@gmail.com>
2019-10-24 19:23:43 +0200

    prviCommit

---
+++ NemkeLegenda/NemkeLegenda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NemkeLegenda
+//
+//  Created by Nikola Lukovic on 24/10/2019.
+//  Copyright © 2019 com.nikola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
